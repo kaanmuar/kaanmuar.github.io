7b1866231911465c8b14772c353e46706fdf6724
Carlos Muñoz <kaanmuar@gmail.com>
2025-07-21 18:52:44 -0500

    Add files via upload

--- cypress.config.js
+++ cypress.config.js
@@ -1,20 +1,43 @@
-// cypress.config.js.
 const { defineConfig } = require('cypress');
+const path = require('path');
+const webpack = require('@cypress/webpack-preprocessor');
+const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 
 module.exports = defineConfig({
-  projectId: 'y95dgu',
   reporter: 'cypress-mochawesome-reporter',
+  projectId: '5p3wz5',
   reporterOptions: {
     charts: true,
-    reportPageTitle: 'Interactive CV - Test Report',
+    reportPageTitle: 'KaanMuar\'s E2E Test Report',
     embeddedScreenshots: true,
     inlineAssets: true,
     saveAllAttempts: false,
   },
   e2e: {
+    baseUrl: 'https://kaanmuar.github.io',
     setupNodeEvents(on, config) {
-      require('cypress-grep/plugin')(on);
-      require('cypress-mochawesome-reporter/plugin')(on);
+      // Grep plugin
+      require('@cypress/grep/src/plugin')(config);
+      // Mochawesome reporter
+      on('before:run', async (details) => {
+        await beforeRunHook(details);
+      });
+      on('after:run', async () => {
+        await afterRunHook();
+      });
+
+      // ** FIX: Add Webpack preprocessor with path alias **
+      const options = {
+        webpackOptions: {
+          resolve: {
+            alias: {
+              '@': path.resolve(__dirname, './cypress'),
+            },
+          },
+        },
+      };
+      on('file:preprocessor', webpack(options));
+
       return config;
     },
   },
